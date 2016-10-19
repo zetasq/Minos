ff862906bd6faca876dcd5313431368c23ec3b1e
Zhu Shengqi <zetasq@gmail.com>
2016-10-19 08:22:31 +0800

    Project setup.

---
+++ Minos-iOS/Minos-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Minos-iOS.h
+//  Minos-iOS
+//
+//  Created by Zhu Shengqi on 19/10/2016.
+//  Copyright © 2016 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Minos-iOS.
+FOUNDATION_EXPORT double Minos_iOSVersionNumber;
+
+//! Project version string for Minos-iOS.
+FOUNDATION_EXPORT const unsigned char Minos_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Minos_iOS/PublicHeader.h>
+
+
---
+++ Minos-macOS/Minos-macOS.h
@@ -0,0 +1,19 @@
+//
+//  Minos-macOS.h
+//  Minos-macOS
+//
+//  Created by Zhu Shengqi on 19/10/2016.
+//  Copyright © 2016 Zhu Shengqi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Minos-macOS.
+FOUNDATION_EXPORT double Minos_macOSVersionNumber;
+
+//! Project version string for Minos-macOS.
+FOUNDATION_EXPORT const unsigned char Minos_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Minos_macOS/PublicHeader.h>
+
+
